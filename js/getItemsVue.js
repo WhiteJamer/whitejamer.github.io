7ed1bed51cb02da0170ad35e40e3347a38ba6256
whitejamer <muxah3258@gmail.com>
2020-01-11 15:14:26 +0300

    Change design and make Vue-rendering from API

---
+++ js/getItemsVue.js
@@ -0,0 +1,20 @@
+Vue.filter('truncate', function (value, limit) {
+  if (value.length > limit) {
+      value = value.substring(0, (limit - 3)) + '...';
+  }
+
+  return value
+})
+
+new Vue({
+	el: '#app',
+	data :{
+		items: null,
+		items2: ['ggg', 'ggg'],
+	},
+	mounted () {
+    axios
+      .get('http://whitejamer.pythonanywhere.com/portfolio/admin/')
+      .then(response => (this.items = response.data))
+  }
+});
